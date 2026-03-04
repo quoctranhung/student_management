af38c640a3360d04e44ab10a6525599cbad70c8f
Tran Hung Quoc <quoc911ndu1819@gmail.com>
2026-03-04 17:56:21 +0700

    Add HomeController to redirect root path to /students

---
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/controller/HomeController.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmut.cse.adsoftweng.lab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/students";
+    }
+}
